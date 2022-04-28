4932f23107d15954bb2d46f25ef32e06dbaaed92
prakharg317 <104030554+prakharg317@users.noreply.github.com>
2022-04-28 20:26:35 +0530

    Time: 11 ms (60.49%), Space: 10.8 MB (71.78%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    
+    void com(int i, vector<vector<int>>&ans, int target,int n,vector<int>&cand,vector<int>&v)
+    {
+       if(i>=n)
+       {
+           if(target==0)
+           {
+               ans.push_back(v);
+               
+           }
+           
+           return;
+       }
+        
+        if(cand[i]<=target)
+        {
+           v.push_back(cand[i]);
+           
+           com(i,ans,target-cand[i],n,cand,v);
+           v.pop_back();
+            
+        }
+        
+        com(i+1,ans,target,n,cand,v);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    vector<vector<int>> combinationSum(vector<int>& cand, int target) 
+    {
+        vector<vector<int>>ans;
+        int n=cand.size();
+        vector<int>v;
+        
+        com(0,ans,target,n,cand,v);
+        return ans;
+        
+        
+    }
+};
