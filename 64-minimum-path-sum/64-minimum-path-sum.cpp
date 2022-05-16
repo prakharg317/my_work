84f9f015aa284931d7bad9a830b1e83ad2ed8475
Prakhar gupta <104030554+prakharg317@users.noreply.github.com>
2022-05-16 12:58:56 +0530

    Time: 11 ms (68.50%), Space: 9.8 MB (74.63%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -17,6 +17,24 @@ public:
         
             
             return dp[m][n]= min(up,right);
+        
+        
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i==0  && j==0)dp[i][j]=grid[i][j];
+                
+                else
+                {
+                    int up=0,left=0;
+                    if(i>0) up= dp[i][j]+dp[i-1][j];
+                    if(j>0)left= dp[i][j]+dp[i][j-1];
+                    dp[i][j]=min(up,left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
             
        
     }
@@ -27,11 +45,29 @@ public:
     {
         int m= grid.size();
         int n= grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
+        //vector<vector<int>>dp(m,vector<int>(n,-1));
+        int dp[m][n];
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i==0  && j==0)dp[i][j]=grid[i][j];
+                
+                else
+                {
+                    int up=grid[i][j],left=grid[i][j];
+                  if(i>0) up= up+dp[i-1][j];
+                  else{up=up+1000000;}  
+                    
+                   if(j>0)left= left+dp[i][j-1];
+                    else{left=left+1000000;}
+                    
+                    dp[i][j]=min(up,left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
         
-        int res= mps(dp,grid,m-1,n-1);
-        
-        return res;
         
     }
 };
