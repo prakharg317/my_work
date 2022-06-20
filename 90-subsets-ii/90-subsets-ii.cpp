c6fab6f3856ae41cdd785a3d7600fcf06dcf0b09
Prakhar gupta <104030554+prakharg317@users.noreply.github.com>
2022-06-20 11:07:43 +0530

    Time: 8 ms (40.13%), Space: 8.5 MB (31.96%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,203 +1,43 @@
 class Solution {
 public:
-    void ss(vector<vector<int>>&ans, vector<int>&v,vector<int>&nums, int ind)
-    {
-        if(ind>=nums.size())
-        {
-            return;
-        }
-       
-        v.push_back(nums[ind]);
-         ans.push_back(v);
-        ss(ans,v,nums,ind+1);
-        v.pop_back();
-        
-        
-         ans.push_back(v);
-        ss(ans,v,nums,ind+1);
-        
-            
-        
-    }
-    
-    
-    
-    
-    
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         
         sort(nums.begin(),nums.end());
+        
         vector<vector<int>>ans;
-        vector<int>v;
+        map<vector<int>,int>mp;
         
+        int n=nums.size();
+        int x= pow(2,n)-1;
         
-        ss(ans,v,nums,0);
-        
-        map<vector<int>, int>mp;
-        
-        for(int i=0; i<ans.size(); i++)
+        for(int i=0;i<=x;i++)
         {
-            mp.insert({ans[i],i});
-        }
-        ans.clear();
-        for(auto it:mp)
-        {
-            ans.push_back(it.first);
+            vector<int>v;
+            if(i==0)
+            {
+                ans.push_back(v);
+                mp.insert({v,i});
+                continue;
+            }
+            
+            int count=0;
+            int a=i;
+            while(a)
+            {
+                if(a&1)
+                {
+                    v.push_back(nums[count]);
+                }
+                count++;
+                a=a>>1;
+            }
+            
+            if(mp.find(v)==mp.end())
+            {
+                ans.push_back(v);
+                mp.insert({v,i});
+            }
         }
         return ans;
-        
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-
-//     public:
-//     vector<int>ans;
-//     //set<vector<int>>s;
-//     void ss(int i, vector<int>&nums,vector<vector<int>>& v1, vector<int> &ds)
-//     {
-//         if(i==nums.size())
-//         {  sort(ds.begin(),ds.end());
-//                v1.push_back(ds);
-//                 return;
-//         }
-        
-//         if(i<nums.size())
-//         {
-//             ds.push_back(nums[i]);
-//             ss(i+1,nums,v1,ds);
-//             ds.pop_back();
-//         }
-        
-//         //int x=i;
-//         int x=i;
-//          while(x<nums.size() && nums[i] == nums[x])
-//          {
-//              x++;
-//          }
-         
-//         ss(x,nums,v1,ds);
-//           //ds.pop_back();
-        
-        
-//     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//     public:
-//     set<vector<int>>s;
-//     vector<vector<int>> subsetsWithDup(vector<int>& nums) 
-//     {
-//         vector<vector<int>> v1;
-//         vector<int>ds;
-//         sort(nums.begin(),nums.end());
-//         ss(0,nums,v1,ds);
-        
-//         for(int i=0; i<v1.size();i++)
-//         {   
-//            s.insert(v1[i]); 
-//         }
-        
-//         v1.clear();
-        
-//         for(auto i:s)
-//         {
-//            v1.push_back(i);
-//         }
-        
-        
-        
-//         return v1;
-        
-//     }
-// };
