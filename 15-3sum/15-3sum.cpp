1157f7c90752a6d3364bbc0909e7b0f673c40cdf
Prakhar gupta <104030554+prakharg317@users.noreply.github.com>
2022-06-20 18:47:14 +0530

    Time: 229 ms (25.47%), Space: 20 MB (68.32%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -6,14 +6,14 @@ public:
         vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
         if(nums.size()==0  ||  nums[0]>0)return ans;
-        
+        vector<int>v;
         for(int i=0;i<nums.size();i++)
         {
             if(( i>0)  && (nums[i]==nums[i-1]))
             {
                 continue;
             }
-            vector<int>v;
+            
             int sum=nums[i];
             
             int j=i+1;
@@ -22,19 +22,27 @@ public:
             while(j<k)
             {
                 
-               if((sum+nums[j]+nums[k])==0)
+               if( (sum+nums[j]+nums[k])==0)
                {
+                   
                    v.push_back(sum);
                    v.push_back(nums[j]);
                    v.push_back(nums[k]);
-                   if(mp.find(v)==mp.end()){
-                    ans.push_back(v);
-                    mp.insert({v,i});
-                      }
+                   // if(mp.find(v)==mp.end()){
+                   //  ans.push_back(v);
+                   //  mp.insert({v,i});
+                   //    }
+                   ans.push_back(v);
                    v.clear();
                   
-                   j++;
+                      while(j<k   && nums[j]==nums[j+1] ) j++;
+                   
+                      while(j<k  &&  nums[k]==nums[k-1])k--;
+                       
+                  j++;
+                   k--;
                    continue;
+                  
                }
                 
                else if((nums[j]+nums[k])<=sum)
