07fc8a15f6f83a7e91fab4a242ecaa5bc65db308
Prakhar gupta <104030554+prakharg317@users.noreply.github.com>
2022-07-14 18:02:22 +0530

    Time: 3 ms (44.06%), Space: 6.5 MB (25.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,98 +1,25 @@
 class Solution {
 public:
-    
-    
-  
-    
-    int uniquePaths(int m, int n) 
+    int up(vector<vector<int>>&dp, int i, int j)
     {
+        if(i==0 && j==0)return 1;
         
+        if(dp[i][j]!=-1)return dp[i][j];
         
-       int dp[m][n];
-        
-       for(int i=0; i<m; i++)
+        int u=0,left=0;
+        if(j>0)
         {
-            for(int j=0; j<n; j++)
-            {
-                if(i==0  && j==0)dp[i][j]=1;
-                
-                  else
-                {
-                    int left=0;
-                      int right=0;
-                   
-                   
-                     if(i>0) left = dp[i-1][j];
-                     if(j>0) right= dp[i][j-1];
-                    
-                     dp[i][j]=  left+right;
-                   
-                }
-            }
+            left=up(dp,i,j-1);
         }
+        if(i>0) u= up(dp,i-1,j);
         
+        return dp[i][j]= u+left;
+    }
+    
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
         
-        
-         return dp[m-1][n-1]; 
-       
-        
-        
-        //return res;
+        return up(dp,m-1,n-1);
         
     }
 };
-
-
-
-
-
-
-
-
-
-
-//////////////////////////using recursion & memoization
-
-
-// class Solution {
-// public:
-    
-    
-    
-//     int up(int m,int n,vector<vector<int>>&dp)
-//     {
-//        if(m==0 && n==0)
-//         {
-//             return 1;
-//         }
-        
-//         if(m<0 || n<0)return 0;
-//         dp[0][0]=1;
-        
-//         if(dp[m][n]!=-1)return dp[m][n];
-//             int left = up(m-1,n,dp) ;
-//              int right = up(m,n-1,dp);
-//             int res= left+right;
-//             return dp[m][n]= res; 
-        
-        
-        
-          
-//     }
-    
-    
-//     int uniquePaths(int m, int n) 
-//     {
-        
-        
-//        vector<vector<int>> dp( m , vector<int> (n, -1)); 
-//         int res= up(m-1,n-1,dp);
-//         return res;
-        
-       
-        
-        
-//         //return res;
-        
-//     }
-// };
