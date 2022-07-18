ba1f6922a431a503a23f3ebac291b6aa05ad9732
Prakhar gupta <104030554+prakharg317@users.noreply.github.com>
2022-07-18 13:08:40 +0530

    Time: 35 ms (95.26%), Space: 12.3 MB (56.56%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,61 +1,57 @@
 class Solution {
 public:
-    void dfs(int i, int j, vector<vector<char>>& grid)
+    void dfs(vector<vector<char>>& grid, int i, int j,int m , int n)
     {
+       grid[i][j]='0';
         
-        grid[i][j]='0';
-        if(j>0)
+        if(j-1>=0)
         {
-           if(grid[i][j-1]=='1')
-           {
-             dfs(i,j-1,grid);  
-           }
-           
-            
-            
-        }
-        
-        if((i+1)<grid.size())
-        {
-            if(grid[i+1][j]=='1')
+            if(grid[i][j-1]=='1')
             {
-              dfs(i+1,j,grid);  
+                dfs(grid,i,j-1,m,n);
             }
             
+            
         }
         
-        if(i>0)
+        if(j+1<n)
         {
-            
-          if(grid[i-1][j]=='1')dfs(i-1,j,grid);  
+            if(grid[i][j+1]=='1')
+            {
+                dfs(grid,i,j+1,m,n);
+            }
         }
         
-        if((j+1)<grid[i].size())
+        if(i-1>=0)
         {
-            
-            if(grid[i][j+1]=='1')dfs(i,j+1,grid);
+            if(grid[i-1][j]=='1')dfs(grid,i-1,j,m,n);
+        }
+        
+        if(i+1<m)
+        {
+           if(grid[i+1][j]=='1')dfs(grid,i+1,j,m,n); 
         }
     }
     
-    
-    
-    
     int numIslands(vector<vector<char>>& grid) {
         
-        int ans=0;
-        
-        for(int i=0; i<grid.size();i++)
+        int count=0;
+        int m=grid.size();
+        int n=grid[0].size();
+        for(int i=0; i<grid.size(); i++)
         {
-            for(int j=0; j<grid[i].size();j++)
+            for(int j=0; j<grid[i].size(); j++)
             {
                 if(grid[i][j]=='1')
-                {   //cout<<"dds";
-                    ans++;
-                    dfs(i,j,grid);
+                {
+                    count++;
+                   dfs(grid, i,j,m,n);
                     
                 }
             }
         }
-        return ans;
+        
+        return count;
+        
     }
 };
