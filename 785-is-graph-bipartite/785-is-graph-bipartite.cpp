0ed09858c098a583737bf400dc3cd22a241b1c20
Prakhar gupta <104030554+prakharg317@users.noreply.github.com>
2022-07-21 13:57:59 +0530

    Time: 21 ms (97.92%), Space: 13.5 MB (83.52%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,59 +1,54 @@
 class Solution {
 public:
+    bool ans=true;
+    
+    void dfs(int i,vector<vector<int>>& graph,vector<int>&col)
+    {
+        if(i==0)col[i]=1;
+        for(auto it:graph[i])
+        {
+          if(col[it]==-1)
+           {
+              if(col[i]==1)
+                {
+                  col[it]=0;
+                  dfs(it,graph,col);
+                }
+                           
+                           
+              else
+              {
+                  col[it]=1;
+                  dfs(it,graph,col);
+              }
+          }
+                        
+          else
+            {
+             if(col[it]==col[i])ans=false;
+            }
+                        
+        }
+    }
+    
     
     bool isBipartite(vector<vector<int>>& graph) {
-        
-//         vector<int>adj[100];
-//         for(int i=0; i<graph.size(); i++)
-//         {
-          
-//             for(int j=0; j<graph[i].size(); j++)
-//             {
-//                 adj[i].push_back(graph[i][j]);
-                
-//             }
-            
-//         }
-        
         int n=graph.size();
-        
         vector<int>col(n,-1);
-        queue<int>q;
+        
         for(int i=0; i<n; i++ )
         {
             if(col[i]==-1)
             {
-                q.push(i);
-                col[i]=1;
-                while(!q.empty())
-                {
-                  int x= q.front();
-                    q.pop();
-                    for(auto it:graph[x])
-                    {
-                        if(col[it]==-1)
-                        {
-                           q.push(it);
-                           if(col[x]==1)
-                           {
-                            col[it]=0;
-                           }
-                           else col[it]=1; 
-                        }
-                        
-                        else
-                        {
-                            if(col[it]==col[x])return false;
-                        }
-                        
-                    }
-                }
+                
+                    
+                dfs(i,graph,col);
             }
         }
         
         
         
-        return true;
+        return ans;
        
         
         
