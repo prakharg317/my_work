4556f0ac9febea42b5173bb62a5b1d21f08c46c3
Prakhar gupta <104030554+prakharg317@users.noreply.github.com>
2022-09-05 13:10:21 +0530

    Time: 1259 ms (18.29%), Space: 11.1 MB (99.10%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    bool sub(const string& pattern, const string& text) {
+    int i, j;
+    for(i = 0, j = 0; i < text.size() && j < 3; ++i) {
+        if(text[i] == pattern[j])
+            ++j;
+    }
+        return j == 3;
+}
+
+   
+    int countPalindromicSubsequence(string s) {
+        int ans = 0;
+        for(char a = 'a'; a <= 'z'; ++a) {
+            for(char b = 'a'; b <= 'z'; ++b) {
+                string str;
+                str += a;
+                str += b;
+                str += a;
+                ans += sub(str, s);
+            }
+        }
+        return ans;
+    
+        
+    }
+};
